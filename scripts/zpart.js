de11398af70b67ed0c24449405ee4c590827a0fb
ankitanshu22 <ankitanshu22@gmail.com>
2020-06-16 16:04:23 +0530

    feed and zpart complete

---
+++ scripts/zpart.js
@@ -0,0 +1,33 @@
+var q1 = document.querySelector(".q1");
+var q2 = document.querySelector(".q2");
+var q3 = document.querySelector(".q3");
+var q4 = document.querySelector(".q4");
+var q1i = document.querySelector(".q1-img");
+var q2i = document.querySelector(".q2-img");
+var q3i = document.querySelector(".q3-img");
+var q4i = document.querySelector(".q4-img");
+
+function showQ2() {
+  q1.style.display = "none";
+  q2.style.display = "block";
+  q1i.style.display = "none";
+  q2i.style.display = "block";
+}
+
+function showQ3() {
+    q2.style.display = "none";
+    q3.style.display = "block";
+    q2i.style.display = "none";
+    q3i.style.display = "block";
+}
+
+function showQ4() {
+    q3.style.display = "none";
+    q4.style.display = "block";
+    q3i.style.display = "none";
+    q4i.style.display = "block";
+}
+
+function changeColor(){
+    btn
+}
