83a869fa3dd2f10f194ca529b3fb7044e376f37e
ankitanshu22 <ankitanshu22@gmail.com>
2020-07-07 01:24:39 +0530

    collapse icon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,10 @@
 const navToggle = () => {
   var e = document.getElementById("burger"),
     t = document.querySelector(".dropdown-content"),
-    n = "images/burger.svg";
+    n = "/src/images/burger.svg";
   e.addEventListener("click", () => {
     t.classList.toggle("dropdown-content-active"),
-      e.src === n ? (e.src = "images/collapse.svg") : (e.src = n);
+      e.src === n ? (e.src = "/src/images/collapse.svg") : (e.src = n);
   });
 };
 navToggle();
